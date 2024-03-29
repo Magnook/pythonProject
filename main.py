50ac625ed45dd9ec5980839083ec9bcd2eadab80
Guilherme Magno <guilhermemagnosilvadecarvalho@gmail.com>
2024-03-29 20:31:38 -0300

    V1.1
    Organização de classes e código

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 import pygame
-
+from avatar import Avatar
 # Inicializando o PYGAME e a Janela
-
 pygame.init()
 
 # Fazendo a Janela se manter me Loop pra não fechar sozinha
 display = pygame.display.set_mode([1280, 720])
 pygame.display.set_caption("Meu Jogo")
 
-# Agrupando os Sprites
-drawGroup = pygame.sprite.Group()
-
-# Importando a imagem do Avatar e adicionando ao Grupo de Sprites
-avatar = pygame.sprite.Sprite(drawGroup)
-avatar.image = pygame.image.load("data/avatar.png")
-avatar.image = pygame.transform.scale(avatar.image, [100, 100])
-avatar.rect = pygame.Rect(100, 100, 100, 100)
-
+# Objetos
+objectGroup = pygame.sprite.Group()
+avatar = Avatar(objectGroup)
 
 # Music
 pygame.mixer.music.load("data/music.wav")
 pygame.mixer.music.play(-1)
+# Sounds
+hit = pygame.mixer.Sound("data/hit.flac")
+
 
 # Base da aplicação
 gameLoop = True
@@ -30,22 +26,15 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 gameLoop = False
-        # Pegando a tecla selecionada do Teclado
-        keys = pygame.key.get_pressed()
 
-        # Teclas de Movimento Horizontal
-        if keys[pygame.K_a]:
-            avatar.rect.x -= 1
-        elif keys[pygame.K_d]:
-            avatar.rect.x += 1
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    hit.play()
 
         # Draw
         display.fill([46, 46, 46])
 
-                        #     x,  y, width, heigth
-        player = pygame.Rect(50, 50, 100, 100)
-        pygame.draw.rect(display, [255, 255, 255, 255], player)
-
-        drawGroup.draw(display)
+        objectGroup.update()
+        objectGroup.draw(display)
 
         pygame.display.update()
